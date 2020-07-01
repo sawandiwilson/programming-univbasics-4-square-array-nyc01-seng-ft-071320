0fb716b59f2d287124567a1f6e0674a9fd6c6d27
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-01 21:59:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
-require 'pry'
 numbers =[1,2,3]
 def square_array(array)
-  binding.pry
+  squared = []
+  count = 0
 while count < array.size do
   squared << (array[count] ** 2)
   count = counter + 1
